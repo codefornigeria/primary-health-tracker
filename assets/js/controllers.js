bea415dfa5020f9b372bdd09c06c68a1ff122557
Tobi oladele <tedlade.gmail.com>
2016-12-08 14:10:17 +0100

    Marker view

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -24,7 +24,6 @@ angular.module('app.controllers', [])
 
     Restangular.all('tracker').getList().then(function(response){
         $scope.tracks = response;
-        console.log(response.plain());
     })
 
     $scope.uploadFiles = function(file, errFiles) {
@@ -69,44 +68,16 @@ angular.module('app.controllers', [])
             console.log(error)
         };
     }
-
-   $scope.areas = [
-		{id: 01, pos:[6.519342, 3.372343], name:'Ikorodu health center', address: 'ikorodu, lagos', rating: '5', type: 'clinic'},
-        {id: 02, pos:[6.505866, 3.367138], name:'Ikeja general hospital', address: 'ikeja, lagos', rating: '4', type: 'hospital'},
-        {id: 03, pos:[6.530316, 3.378114], name:'Military hospital', address: 'yaba, lagos', rating: '3', type: 'genaral'},
-        {id: 04, pos:[6.520614, 3.352457], name:'Eko hospital', address: 'ikeja, lagos', rating: '2', type: 'maternity'},
-        {id: 05, pos:[6.522490, 3.382734], name:'Oak hospital', address: 'ikorodu, lagos', rating: '1', type: 'hospital'},
-	];
-    $scope.yellowareas = [
-        {id: 01, pos:[6.522490, 3.382734], name:'yaba', address: 'Yaba college of technology'},
-        {id: 02, pos:[6.522570, 3.373219], name:'yaba', address: 'Yaba college of technology'},
-        {id: 03, pos:[6.508156, 3.380825], name:'yaba', address: 'Yaba college of technology'},
-        {id: 04, pos:[6.508698, 3.384125], name:'yaba', address: 'Yaba college of technology'},
-        {id: 05, pos:[6.519765, 3.354342], name:'yaba', address: 'Yaba college of technology'}
-    ];
-    $scope.blues = [
-        {id: 01, pos:[6.522390, 3.386834], name:'yaba', address: 'Yaba college of technology'},
-        {id: 02, pos:[6.527570, 3.372319], name:'yaba', address: 'Yaba college of technology'},
-        {id: 03, pos:[6.589656, 3.380457], name:'yaba', address: 'Yaba college of technology'},
-        {id: 04, pos:[6.503498, 3.344121], name:'yaba', address: 'Yaba college of technology'},
-        {id: 05, pos:[6.515768, 3.354148], name:'yaba', address: 'Yaba college of technology'}
-    ];
-    $scope.magentas = [
-        {id: 01, pos:[6.535490, 3.562734], name:'yaba', address: 'Yaba college of technology'},
-        {id: 02, pos:[6.578570, 3.323419], name:'yaba', address: 'Yaba college of technology'},
-        {id: 03, pos:[6.501256, 3.308825], name:'yaba', address: 'Yaba college of technology'},
-        {id: 04, pos:[6.580698, 3.345125], name:'yaba', address: 'Yaba college of technology'},
-        {id: 05, pos:[6.571765, 3.353642], name:'yaba', address: 'Yaba college of technology'}
-    ];
 	
     $scope.seeDetails = function(evt, obj, type) {
-        $scope.details = true;
-        $scope.selectedPoint = obj;
+        $scope.point = obj;
+        console.log($scope.point.rating);
+        $scope.openTrack = true;
+        // Restangular.one('track-hospitals', hospital).get().then(function(results){
+        //     $scope.results = results;
+        //     console.log(results.plain());
+        // })
     }
-
-    // NgMap.getMap().then(function(map) {
-    	
-    // });
 
     $scope.search = function() {
         var address = $scope.track.name;
@@ -125,8 +96,6 @@ angular.module('app.controllers', [])
     $scope.addLocation = function(result) {
         $scope.track.name = result.lga;
         $scope.track.lga = result.lga;
-        // $scope.location.latitude = result.geometry.location.lat;
-        // $scope.location.longitude = result.geometry.location.lng;
         $scope.searching = false;
         $scope.showDetail();
     }
